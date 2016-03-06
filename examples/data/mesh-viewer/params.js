a7a53198c64d0c1df5fe72b98e0747852cd9f75f
Giuseppe Portelli <gportelli@e-mentor.it>
2016-03-06 23:51:13 +0100

    light bulb materials updated

--- examples/data/mesh-viewer/params.js
+++ examples/data/mesh-viewer/params.js
@@ -1,14 +1,16 @@
 {
 	meshes: [
-		{ url: "light-bulb.obj", name: "Light Bulb", y: 0, rx:90, scale: 0.18,
-		  materials: [
-		  	{ color: 0xff0000 },
-		  	{ color: 0x00ff00 },
-	  	  ]
+		{ 
+			url: "dice.obj", name: "Dice", y: 0, ry: -55, scale: 1,
+		  	materials: [{ diffuseMap: "diceDiffuseMap.jpg", normalMap: "dice-normal-clean.png" }]
 		},
 		
-		{ url: "dice.obj", name: "Dice", y: 0, ry: -55, scale: 1,
-		  materials: [{ diffuseMap: "diceDiffuseMap.jpg", normalMap: "dice-normal-clean.png" }]
+		{ 
+			url: "light-bulb.obj", name: "Light Bulb", y: 0, rx:90, scale: 0.18,
+		  	materials: [
+			  	{ color: 0xaaaaaa },
+			  	{ color: 0xc0a84a },
+	  	  	]
 		}
 	],
 
